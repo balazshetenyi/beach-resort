e056aabd8bc48d2e29a95c9b514d42660a9183ab
balazshetenyi <balazshetenyi82@gmail.com>
2020-06-17 17:07:04 +0100

    connected to contentful

--- src/Context.js
+++ src/Context.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react'
-import items from './data'
+// import items from './data'
+import Client from './Contentful'
 
 const RoomContext = React.createContext()
-
 
 class RoomProvider extends Component {
     state ={
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: true,
+        type: 'all',
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
     }
 
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: 'beachResortRoom',
+                order: '-fields.price'
+            })
+            let rooms = this.formatData(response.items)
+            let featuredRooms = rooms.filter(room => room.featured === true)
+            let maxPrice = Math.max(...rooms.map(item => item.price))
+            let maxSize = Math.max(...rooms.map(item => item.size))
+
+            this.setState({
+                rooms,
+                featuredRooms,
+                sortedRooms: rooms,
+                loading: false,
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
     componentDidMount() {
-        let rooms = this.formatData(items)
-        let featuredRooms = rooms.filter(room => room.featured === true)
-        this.setState({
-            rooms, 
-            featuredRooms, 
-            sortedRooms: rooms,
-            loading: false
-        })
+        this.getData()
     }
 
     formatData(items) {
@@ -34,9 +61,61 @@ class RoomProvider extends Component {
         return tempItems
     }
 
+    getRoom = slug => {
+        let tempRooms = [...this.state.rooms]
+        const room = tempRooms.find(room => room.slug === slug)
+        return room
+    }
+
+    handleChange = event => {
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = event.target.name
+        
+        this.setState({
+            [name]: value
+        }, this.filterRooms)
+    }
+
+    filterRooms = () => {
+        let { rooms, capacity, type, price, minSize, maxSize, breakfast, pets } = this.state
+        // all the rooms
+        let tempRooms = [...rooms]
+        // transform value
+        capacity = parseInt(capacity)
+        price = parseInt(price)
+
+        // filter by type
+        if (type !== 'all') {
+            tempRooms = tempRooms.filter(room => room.type === type)
+        }
+        // filter by capacity
+        if (capacity !== 1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+        }
+        // filter by price
+        tempRooms = tempRooms.filter(room => room.price <= price)
+        // filter by room size
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+        // filter by breakfast
+        if (breakfast) {
+            tempRooms = tempRooms.filter(room => room.breakfast === true)
+        }
+        // filter by pets
+        if (pets) {
+            tempRooms = tempRooms.filter(room => room.pets === true)
+        }
+        // change state
+        this.setState({
+            sortedRooms: tempRooms
+        })
+    }
+
     render() {
         return (
-            <RoomContext.Provider value={{...this.state}}>
+            <RoomContext.Provider 
+                value={{...this.state, getRoom: this.getRoom, handleChange: this.handleChange}}
+            >
                 {this.props.children}
             </RoomContext.Provider>
         )
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,9 +2,11 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import RoomsContainer from '../components/RoomsContainer';
 
 function Rooms() {
     return (
+        <>
         <Hero hero='roomsHero'>
             <Banner title='our rooms' >
                 <Link to='/' className='btn-primary'>
@@ -12,6 +14,8 @@ function Rooms() {
                 </Link>
             </Banner>
         </Hero>
+        <RoomsContainer />
+        </>
     )
 }
 
